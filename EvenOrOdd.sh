38547e49579bb81197c3de06bd6cb5ce0b427ebf
AviLibson <avixoox2011@gmail.com>
2025-03-06 02:38:22 +0200

    Update EvenOrOdd.sh

--- EvenOrOdd.sh
+++ EvenOrOdd.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Prompt the user to enter a number
-read -p "Enter a number: " num
+# Check if an argument is provided
+if [[ -z "$1" ]]; then
+    echo "No input provided. Please enter a number."
+    exit 1
+fi
 
-# Check if the input is a number
-if [[ ! $num =~ ^[0-9]+$ ]]; then
+# Assign input argument to a variable
+num="$1"
+
+# Check if the input is a valid integer (including negative numbers)
+if ! [[ "$num" =~ ^-?[0-9]+$ ]]; then
     echo "Invalid input. Please enter a valid number."
     exit 1
 fi
